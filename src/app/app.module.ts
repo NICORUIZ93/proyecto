7c0943a3d7ad6551f1f62d9dd581b805c2fa8553
NICORUIZ93 <nicolasenriqueruiz93@hotmail.com>
2020-10-26 00:48:51 -0500

    modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HojaComponent } from '../app/Components/hoja/hoja.component';
-import { HomeComponent } from './Components/home/home.component';
 import { AboutComponent } from './Components/about/about.component';
 import { RouterRoutingModule } from './router-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, HojaComponent, HomeComponent, AboutComponent],
+  declarations: [AppComponent, HojaComponent, AboutComponent],
   imports: [BrowserModule, BrowserAnimationsModule, RouterRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/router-routing.module.ts
+++ src/app/router-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './Components/about/about.component';
 import { HojaComponent } from './Components/hoja/hoja.component';
-import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'hoja', component: HojaComponent },
+  { path: 'home', component: HojaComponent },
   { path: 'about', component: AboutComponent },
 ];
 
